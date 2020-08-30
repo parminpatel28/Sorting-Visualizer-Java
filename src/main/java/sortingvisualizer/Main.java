182ee5693ce7242cf44c3bb3fc5ec514f97eefdb
Parmin Patel <parmin@Parmins-MacBook-Pro.local>
2020-08-30 16:09:05 -0400

    addede a random array. changed the delay. added the title section to the file

--- src/main/java/sortingvisualizer/Main.java
+++ src/main/java/sortingvisualizer/Main.java
@@ -13,6 +13,7 @@ import javax.swing.JMenu;
 import javax.swing.JMenuBar;
 import javax.swing.JMenuItem;
 import javax.swing.JPanel;
+import javax.swing.JTextField;
 
 /**
  *
@@ -20,31 +21,42 @@ import javax.swing.JPanel;
  */
 public class Main extends JPanel {
 
-    public static int[] ar = {12, 14, 123, 45, 89, 12, 34, 45, 65, 67, 80, 34, 150};
+    //public static int[] ar = {12, 14, 123, 45, 89, 12, 34, 45, 65, 67, 80, 34, 150};
+    public static int[] ar = new int[65];
 
     /**
      * @param args the command line arguments
      */
+    
     public static void main(String[] args) {
         // TODO code application logic here
-        JFrame a = new JFrame();
+        //Frame setting up
+        JFrame a = new JFrame("Sorting Visualizer");
         Main b = new Main();
         a.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         a.setSize(1000, 600);
-
+        JTextField newTitle = new JTextField("Some Title"); 
+        newTitle.setBounds(80, 40, 225, 20);
         a.setVisible(true);
 
         a.add(b);
 
         a.setExtendedState(JFrame.MAXIMIZED_BOTH);
-
         Random rd = new Random();
-        int algorithm = 2;//rd.nextInt(3);
+        for(int p = 0; p < 65;p++){
+            ar[p] = rd.nextInt(550);
+        }
+        
+        
+
+        //a random number to decide with algorithm to use
+        int algorithm = rd.nextInt(3);
         System.out.println(algorithm);
 
         switch (algorithm) {
             //code for Bubble Sort
             case 0:
+                a.setTitle("Sorting Visualizer - Bubble Sort");
                 for (int x = 0; x < b.ar.length; x++) {
                     for (int y = 0; y < b.ar.length - 1; y++) {
                         int temp = 0;
@@ -55,7 +67,7 @@ public class Main extends JPanel {
                             b.ar[y] = temp;
                         }
                         try {
-                            Thread.sleep(300);
+                            Thread.sleep(10);
                         } catch (Exception ex) {
                         }
                         b.repaint();
@@ -65,6 +77,7 @@ public class Main extends JPanel {
                 break;
              //code for Insertion Sort
             case 1:
+                a.setTitle("Sorting Visualizer - Insertion Sort");
                 for (int i = 0; i < ar.length; i++) {
                     int index = ar[i];
                     int j = i - 1;
@@ -75,16 +88,15 @@ public class Main extends JPanel {
                     }
                     ar[j + 1] = index;
                     try {
-                        Thread.sleep(300);
+                        Thread.sleep(100);
                     } catch (Exception ex) {
                     }
                     b.repaint();
                 }
                 break;
             // code for Selection Sort 
-            case 2:
-                
-
+            case 2:                
+                a.setTitle("Sorting Visualizer - Selection Sort");
                 for (int step = 0; step < ar.length - 1; step++) {
                     int min_idx = step;
 
@@ -100,7 +112,7 @@ public class Main extends JPanel {
                     ar[min_idx] = temp;
                 
                 try {
-                    Thread.sleep(300);
+                    Thread.sleep(100);
                 } catch (Exception ex) {
                 }
                 b.repaint();
@@ -109,8 +121,8 @@ public class Main extends JPanel {
         }
 
     }
-
-    int xloc = 0, yloc = 700, width = 40;
+    //decides the dimensions on the arrays on the page
+    int xloc = 0, yloc = 700, width = 20;
 
     @Override
     public void paintComponent(Graphics g) {
@@ -119,7 +131,7 @@ public class Main extends JPanel {
         draw(g);
 
     }
-
+    // draws the array that need to be sorted 
     public void draw(Graphics g) {
         for (int x = 0; x < ar.length; x++) {
             xloc += width + 1;
